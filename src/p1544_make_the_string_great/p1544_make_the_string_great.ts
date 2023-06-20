f04ed142a6e6bb766912619b787a4535ba89f5aa
Gustavo Basso <gubasso@cwnt.io>
2023-06-20 14:12:51 -0300

    (locked) Issue: p1544_make_the_string_great (#1)

--- src/p1544_make_the_string_great/p1544_make_the_string_great.ts
+++ src/p1544_make_the_string_great/p1544_make_the_string_great.ts
@@ -1,23 +1,12 @@
-function isOneUpperOtherLower(c1: string | undefined, c2: string | undefined): boolean {
-  if (c1 === undefined && c2 === undefined) { return true }
-  if (c1 === undefined || c2 === undefined) { return false }
-  if (c1 === c2) { return false }
-  return c1.toUpperCase() === c2 || c1 === c2.toUpperCase()
-}
-
 export function makeGood(s: string): string {
-  let stack = []
+  let stack: string[] = []
   for (let c of s) {
     let last: string | undefined = stack.at(-1)
-    let current: string | undefined = c
-
-    if (!isOneUpperOtherLower(last, current)) { stack.push(c) }
-
-    while (isOneUpperOtherLower(last, current)) {
-      current = stack.pop()
-      last = stack.at(-1)
+    if (!last || last === c || c.toUpperCase() !== last.toUpperCase()) {
+      stack.push(c)
+    } else {
+      stack.pop()
     }
-
   }
   return stack.join("")
 };
