bfcda7455ba2b279366f52195daca696cf545434
Gustavo Basso <gubasso@cwnt.io>
2023-06-26 17:10:56 -0300

    p104_maximum_depth_of_binary_tree adjusted

---
+++ src/p104_maximum_depth_of_binary_tree/README.md
@@ -0,0 +1,37 @@
+# p104_maximum_depth_of_binary_tree
+
+## Iteractive approach
+
+```
+function maxDepth(root: TreeNode | null): number {
+  let ans = 0
+  let stack: [TreeNode | null, number][] = [[root, 1]]
+
+  while (stack.length) {
+    let [node, depth] = stack.pop() || [null,0]
+    ans = Math.max(ans, depth)
+    if (node?.left) {
+      stack.push([node?.left, depth + 1])
+    }
+    if (node?.right) {
+      stack.push([node?.right, depth + 1])
+    }
+  }
+
+  return ans
+};
+```
+
+## Recursive approach
+
+```
+function maxDepth(root: TreeNode | null): number {
+  // if (root == null) return 0;
+  // let maxLeft = maxDepth(root.left)
+  // let maxRight = maxDepth(root.right)
+  // return Math.max(maxLeft, maxRight) + 1
+  if (!root) return 0;
+  return 1 + Math.max(maxDepth(root.right), maxDepth(root.left));
+};
+```
+
--- src/p104_maximum_depth_of_binary_tree/p104_maximum_depth_of_binary_tree.ts
+++ src/p104_maximum_depth_of_binary_tree/p104_maximum_depth_of_binary_tree.ts
@@ -18,15 +18,23 @@ export function vecToBinaryTree(vec: (number | null)[], j?: number): TreeNode | 
   let tree = new TreeNode(val)
   tree.left = vecToBinaryTree(vec, 2*i+1)
   tree.right = vecToBinaryTree(vec, 2*i+2)
-  console.log(tree)
   return tree
 }
 
 export function maxDepth(root: TreeNode | null): number {
-  // if (root == null) return 0;
-  // let maxLeft = maxDepth(root.left)
-  // let maxRight = maxDepth(root.right)
-  // return Math.max(maxLeft, maxRight) + 1
-  if (!root) return 0;
-  return 1 + Math.max(maxDepth(root.right), maxDepth(root.left));
+  let ans = 0
+  let stack: [TreeNode | null, number][] = [[root, 1]]
+
+  while (stack.length) {
+    let [node, depth] = stack.pop() || [null,0]
+    ans = Math.max(ans, depth)
+    if (node?.left) {
+      stack.push([node?.left, depth + 1])
+    }
+    if (node?.right) {
+      stack.push([node?.right, depth + 1])
+    }
+  }
+
+  return ans
 };
