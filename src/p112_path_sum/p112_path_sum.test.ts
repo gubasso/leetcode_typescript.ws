825e87ace7145290b8180ec461972d3e961fc92b
Gustavo Basso <gubasso@cwnt.io>
2023-06-30 15:45:00 -0300

    p112_path_sum update

--- src/p112_path_sum/p112_path_sum.test.ts
+++ src/p112_path_sum/p112_path_sum.test.ts
@@ -1,12 +1,15 @@
 import {describe, expect, test} from '@jest/globals';
-import { vecToBinaryTree, hasPathSum } from './p112_path_sum';
+import {  vecToBinaryTreeIter, vecToBinaryTree, hasPathSum } from './p112_path_sum';
 
 describe('p112_path_sum', () => {
 
   test('utils', () => {
     let input = [3,9,20,null,null,15,7]
     let tree = vecToBinaryTree(input)
-    // console.log(tree)
+    let tree_iter = vecToBinaryTreeIter(input)
+    let input2 = [5,4,8,11,null,13,4,7,2,null,null,null,1]
+    let tree2 = vecToBinaryTree(input2)
+    let tree2_iter = vecToBinaryTreeIter(input2)
   });
 
   test('t1', () => {
--- src/p112_path_sum/p112_path_sum.ts
+++ src/p112_path_sum/p112_path_sum.ts
@@ -16,6 +16,30 @@ export function vecToBinaryTree(vec: (number | null)[], j?: number): TreeNode | 
   return new TreeNode(vec[i]||0, vecToBinaryTree(vec, 2*i + 1), vecToBinaryTree(vec, 2*i + 2))
 }
 
+export function vecToBinaryTreeIter(vec: (number | null)[], j?: number): TreeNode | null {
+  if (vec.length == 0 || vec[0] == null) { return null }
+  let tree = new TreeNode(vec[0])
+  let stack: [TreeNode, number][] = [[tree, 0]]
+  while (stack.length > 0) {
+    let [node, idx] = stack.pop() || [new TreeNode(), 0]
+    let left_idx = 2*idx + 1
+    let right_idx = 2*idx + 2
+    let left_val = vec[left_idx]
+    let right_val = vec[right_idx]
+    if (left_val) {
+      let node_left = new TreeNode(left_val)
+      node.left = node_left
+      stack.push([node_left, left_idx])
+    }
+    if (right_val) {
+      let node_right = new TreeNode(right_val)
+      node.right = node_right
+      stack.push([node_right, right_idx])
+    }
+  }
+  return tree
+}
+
 export function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
   if (!root) return false;
   let node: TreeNode = root as TreeNode;
